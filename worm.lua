a7a612c68f0c33483c3d52a287ca93236aea6814
TheGreatOath <pertuisance@gmail.com>
2025-04-10 15:52:04 +0300

    Update worm.lua

--- worm.lua
+++ worm.lua
@@ -72,7 +72,7 @@ local ConfigurationExtension = ".rfld"
 local settingsTable = {
 	General = {
 		-- if needs be in order just make getSetting(name)
-		RayfieldOpen = {Type = 'bind', Value = 'K', Name = 'Wormfield Keybind'},
+		WormfieldOpen = {Type = 'bind', Value = 'K', Name = 'Wormfield Keybind'},
 		-- buildwarnings
 		-- wormfieldprompts
 
@@ -109,8 +109,8 @@ local function loadSettings()
 			-- for debug in studio
 			if useStudio then
 				file = [[
-		{"General":{"wormfieldOpen":{"Value":"K","Type":"bind","Name":"Wormfield Keybind","Element":{"HoldToInteract":false,"Ext":true,"Name":"Wormfield Keybind","Set":null,"CallOnChange":true,"Callback":null,"CurrentKeybind":"K"}}},"System":{"usageAnalytics":{"Value":false,"Type":"toggle","Name":"Anonymised Analytics","Element":{"Ext":true,"Name":"Anonymised Analytics","Set":null,"CurrentValue":false,"Callback":null}}}}
-	]]
+		--{"General":{"wormfieldOpen":{"Value":"K","Type":"bind","Name":"Wormfield Keybind","Element":{"HoldToInteract":false,"Ext":true,"Name":"Wormfield Keybind","Set":null,"CallOnChange":true,"Callback":null,"CurrentKeybind":"K"}}},"System":{"usageAnalytics":{"Value":false,"Type":"toggle","Name":"Anonymised Analytics","Element":{"Ext":true,"Name":"Anonymised Analytics","Set":null,"CurrentValue":false,"Callback":null}}}}
+	--]]
 			end
 
 
