992eaa829e5b4db05bc3dba7ea83ba3e64029cc1
TheGreatOath <pertuisance@gmail.com>
2025-04-10 16:06:01 +0300

    Update worm.lua

--- worm.lua
+++ worm.lua
@@ -76,6 +76,10 @@ local settingsTable = {
 		-- buildwarnings
 		-- wormfieldprompts
 	},
+	System = {
+		usageAnalytics = {Type = 'toggle', Value = true, Name = 'Anonymised Analytics'},
+	}
+}
 
 local HttpService = getService('HttpService')
 local RunService = getService('RunService')
